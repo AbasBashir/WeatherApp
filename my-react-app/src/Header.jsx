b44d43c01a2bb786ba154203f7863219c09db22d
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:05:17 +0200

    stateful variable + setter function used to manage the opening and closing of future searchbar

---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,9 @@
+import React,{useState} from 'react';
+
+function Header() {
+
+    const [isSearchOpen, setIsSearchOpen] = useState(false); // stateful variable + setter function used to manage the opening and closing of searchbar
+
+}
+
+export default Header
