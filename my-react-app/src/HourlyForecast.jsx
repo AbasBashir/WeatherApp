40a95d81a89c406f7fe5198eb2adb6fb844ef8a4
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 18:27:05 +0200

    Created the structure to display hourly forecast.

--- my-react-app/src/HourlyForecast.jsx
+++ my-react-app/src/HourlyForecast.jsx
@@ -10,7 +10,20 @@ function HourlyForecast({hourlyForecastData, degreeStatus}) {
 
     return (
         <div className='w-full max-w-7xl mx-auto relative top-20 sm:top-52 pb-20'>
-
+            <div className='flex items-center justify-start mt-10 sm:mt-6'>
+                <h2 className='pl-2 text-white text-2xl font-extralight w-full text-center sm:text-left'>Hourly Forecast</h2>
+            </div>
+            <hr className='mt-1'/>
+            
+            <div className='flex flex-col items-center space-y-6 sm:space-y-0 sm:flex-row justify-around text-center text-white mt-10 sm:mt-6'>
+                {firstFiveHours.map((hour, index) => (
+                <div key={index} className='flex flex-col items-center justify-center space-y-1 text-lg border-[1px] border-white rounded-md p-6 sm:border-none'>
+                    <p className='text-white text-md  font-extralight'>{hour.time.slice(10)} AM</p>
+                    <img src={`https:${hour.condition.icon}`} alt='Weather Icon' className='w-18 h-18 '/>
+                    <p>{degreeStatus ? hour.temp_c + "°C" : hour.temp_f + "°F"}</p>
+                </div>
+                ))}
+            </div>
         </div>
     )
 }
