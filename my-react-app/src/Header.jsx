eb9083c8476d344f67a11ffcd672e031055e08cc
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:06:06 +0200

    function to update the stateful variable

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -4,6 +4,12 @@ function Header() {
 
     const [isSearchOpen, setIsSearchOpen] = useState(false); // stateful variable + setter function used to manage the opening and closing of searchbar
 
+    // function to update the stateful variable
+    const toggleSearch = () => {
+        setIsSearchOpen(!isSearchOpen);
+    };
+
+
 }
 
 export default Header
