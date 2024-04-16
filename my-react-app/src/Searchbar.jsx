dcdc318a9de1a6b7f47318dcdd15371cf35483c7
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:27:10 +0200

    Created a stateful variable setter used for displaying text in the searchbar when a user types in the searchbar

--- my-react-app/src/Searchbar.jsx
+++ my-react-app/src/Searchbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 function Searchbar() {
+
+    const [search, setSearch] = useState(""); // This is a stateful variable setter used for displaying text in the searchbar when a user types
+
   return (
     <div>
       
