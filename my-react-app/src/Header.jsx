19cde9aaeceebf85fe435f98be9e9ef8217128d3
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:25:18 +0200

    Created Searchbar component and also established a conditional for showing the component in the Header component

--- my-react-app/src/Header.jsx
+++ my-react-app/src/Header.jsx
@@ -20,6 +20,9 @@ function Header() {
                 </div>
                 <span><img src={searchLogo} alt="search-icon" className='w-5 h-5 mt-1 cursor-pointer transition ease-out duration-100 hover:scale-125' onClick={toggleSearch}/></span>
             </div>
+
+            {/* isSearchOpen is placed here as a conditional to display the Searbar component only when isSearchOpen is set to true */}
+            {isSearchOpen && <Searchbar/>} 
         </div>
     )
 
