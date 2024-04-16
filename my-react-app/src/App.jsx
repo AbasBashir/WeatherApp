11c18f5af5ccc456668e40385da031721c452e42
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:46:38 +0200

    Storing base_url and API key in variables inside App component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import Header from './Header';
 
 function App() {
 
+  const apiKey = 'cc3a69cee89d442bbef103435240904';
+  const base_Url = 'https://api.weatherapi.com/v1'
+
   return (
     <>
       <div className="bg-gradient-to-br from-cyan-700 to-red-700 min-h-screen pt-50">
