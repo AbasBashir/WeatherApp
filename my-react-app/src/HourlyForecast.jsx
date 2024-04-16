47bc3375ab73e99b060e81af38c592c598226f09
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 18:26:00 +0200

    Established an if statement that will check if data does exists and also created a variable that will  store  the 5 first hours inside firstFiveHours.

--- my-react-app/src/HourlyForecast.jsx
+++ my-react-app/src/HourlyForecast.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 
 function HourlyForecast({hourlyForecastData, degreeStatus}) {
 
+    if (!hourlyForecastData) {
+        return null; 
+    }
+    const firstFiveHours = hourlyForecastData.firstHour.slice(0, 5); // Here we are using the slice() method on firstHour property and we essentially store the 5 first hours inside firstFiveHours
 
     return (
         <div className='w-full max-w-7xl mx-auto relative top-20 sm:top-52 pb-20'>
