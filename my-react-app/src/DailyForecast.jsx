3dcabcba6258a5bb95ad871d66fcc7aa01afba82
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 18:31:22 +0200

    Established an if statement that will check if dailyForecast data exists or not, if it does not exist it will return null. Also created a variable that will store the first 5 days which is in forecastDays

--- my-react-app/src/DailyForecast.jsx
+++ my-react-app/src/DailyForecast.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 function DailyForecast({dailyForecastData, degreeStatus}) {
 
+    if (!dailyForecastData) {
+        return null; 
+    }
+
+    const firstFiveDays = dailyForecastData.forecastDays; // The first 5 days which is in forecastDays property is stored in firstFiveDays variable
 
     return (
         <div className='w-full max-w-7xl mx-auto pt-18 sm:pt-40 pb-20'>
