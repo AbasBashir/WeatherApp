8c6a35b0d7db7a522f7024f3513f9928f3aa7617
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:29:01 +0200

    function which updates the search state depending on user input and also calls the fetchData()

--- my-react-app/src/Searchbar.jsx
+++ my-react-app/src/Searchbar.jsx
@@ -5,6 +5,13 @@ function Searchbar() {
     const [search, setSearch] = useState(""); // This is a stateful variable setter used for displaying text in the searchbar when a user types
     const [searchResult, setSearchResult] = useState([]); // This is used for updating and storing the latest weather data depending on what city the user selects but searchResult is also used for displaying search results 
 
+    // important function which updates the search state depending on user input and also calls the fetchData() with the input value from the user
+    function searchUpdate(event){
+        setSearch(event.target.value);
+        const value = event.target.value;
+        fetchData(value);
+      }
+
   return (
     <div>
       
