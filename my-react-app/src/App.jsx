79c74db39e093b13688cbc5f3001cc6143eff9ee
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:14:48 +0200

    importing Header component to app and rendering it on the browser

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,13 @@
+import React,{useState, useEffect} from 'react';
+import Header from './Header';
 
 function App() {
 
   return (
     <>
-    
+      <div className="bg-gradient-to-br from-cyan-700 to-red-700 min-h-screen pt-50">
+        <Header></Header>
+      </div>
     </>
   )
 }
