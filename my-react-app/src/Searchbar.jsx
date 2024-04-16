0884fc0bb339bea16ebc725c02a1335c7b67f6b8
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:28:14 +0200

    Created another stateful variable setter that will be used for  updating and storing the latest weather data

--- my-react-app/src/Searchbar.jsx
+++ my-react-app/src/Searchbar.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function Searchbar() {
 
     const [search, setSearch] = useState(""); // This is a stateful variable setter used for displaying text in the searchbar when a user types
+    const [searchResult, setSearchResult] = useState([]); // This is used for updating and storing the latest weather data depending on what city the user selects but searchResult is also used for displaying search results 
 
   return (
     <div>
