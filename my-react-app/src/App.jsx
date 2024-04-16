d31339e87e33b41b012871215d94d66f9b67abbf
AbasBashir <MrAbasBashir@gmail.com>
2024-04-16 17:47:50 +0200

    Establishing different useState + setter functions for later use and currently

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -4,7 +4,15 @@ import Header from './Header';
 function App() {
 
   const apiKey = 'cc3a69cee89d442bbef103435240904';
-  const base_Url = 'https://api.weatherapi.com/v1'
+  const base_Url = 'https://api.weatherapi.com/v1';
+
+  const [weatherData, setWeatherData] = useState(null); // this is the stateful setter variable that stores all data related to weather
+  const [sys, setSys] = useState(null); // state to store sunrise/sunset data
+  const [hourlyForecastData, setHourlyForecastData] = useState(null); // State to store hourly forecast data
+  const [dailyForecastData, setDailyForecastData] = useState(null); // State to store daily forecast data
+  const [degreeStatus, setDegreeStatus] = useState(true); // State to store either celsius or fahrenheit on a condition, intially degreeStatus will have a boolean value of true whihc represents °C
+
+
 
   return (
     <>
